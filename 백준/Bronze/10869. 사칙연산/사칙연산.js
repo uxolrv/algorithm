f8dabbb03835565c2264a20498a6375fa1d64102
SeYeon <107875909+uxolrv@users.noreply.github.com>
2022-09-08 23:36:34 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync("/dev/stdin").toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
