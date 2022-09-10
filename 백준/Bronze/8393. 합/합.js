3a4bd9a2920d79d3c3fd3a720bbe2730260f1294
김세연 <107875909+uxolrv@users.noreply.github.com>
2022-09-10 23:31:36 +0900

    [Bronze V] Title: 합, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync("/dev/stdin").toString();
+const n = parseInt(input)
+let result = 0;
+
+for (let i = 0; i <= n; i++) {
+    result += i
+}
+
+console.log(result)
