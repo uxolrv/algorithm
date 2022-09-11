442ef6e499d0011d3ed505b0e92188e7ae83a3d1
김세연 <107875909+uxolrv@users.noreply.github.com>
2022-09-11 12:22:15 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1340 ms, Memory: 174944 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync("/dev/stdin").toString().split('\n');
+
+// input[0] 테스트 케이스 개수
+
+let result = 0;
+
+for (let i = 1; i <= Number(input[0]); i++) {
+    let items = input[i].split(' ')
+    let sum = Number(items[0]) + Number(items[1])
+    result = `${result + sum}\n`
+}
+
+console.log(result.slice(0, -1))
