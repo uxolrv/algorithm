1069a53a427beaeef8619820a4c091a98b2cf20b
김세연 <107875909+uxolrv@users.noreply.github.com>
2022-10-09 15:54:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,40 @@
+function solution(n, arr1, arr2) {
+    // 이진수로 바꾸는 함수
+    function toBinaryNum (arr) {
+        // 이진수로 변환
+        let binaryArr = arr.map(el => el.toString(2))
+        
+        /*
+        'binaryArr1' [ '1001', '10100', '11100', '10010', '1011' ]
+        'binaryArr2' [ '11110', '1', '10101', '10001', '11100' ]
+        */
+        
+        // 앞에 0 추가하여, 자리수 맞춰주기
+        // padStart() ===> 현재 문자열의 시작을 다른 문자열로 채워, 주어진 길이를 만족하는 새로운 문자열을 반환
+        binaryArr = binaryArr.map(el => el.padStart(n, "0"))
+        return binaryArr
+    } 
+  
+    const binary1 = toBinaryNum(arr1)
+    const binary2 = toBinaryNum(arr2)
+    let result = [];
+    // 1은 벽, 0은 공백
+    // 하나라도 벽이면 벽, 둘다 공백이면 공백
+
+    for (let i = 0; i < n; i++) {
+        // 한 줄을 해독한 결과를 담을 str
+        let done = '';
+        
+        for (let j = 0; j < binary1[i].length; j++) {
+        const num1 = binary1[i][j]
+        const num2 = binary2[i][j]
+        
+        // 둘 중 하나라도 1 이라면 #, 아니면 " "
+        if (num1 === "1" || num2 === "1") done += "#"
+        else done += " "
+        }
+        result.push(done)
+    }
+    
+    return result
+}
