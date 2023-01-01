6dde42f805e251955679ef0eb2be80ed29b064f2
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-01-01 23:38:53 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,9 +1,5 @@
 function solution(array, n) {
-    let count = 0
-    
-    for (let num of array) {
-        if (num === n) count++
-    }
+    const count = array.filter(num => num === n).length
     
     return count;
 }
