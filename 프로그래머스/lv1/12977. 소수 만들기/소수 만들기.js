7b15bc4c4756ca6f453ccd88135fb5bd8dc403f0
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-01-02 01:30:09 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,25 @@
+function solution(nums) {
+  let count = 0
+  
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = i + 1; j < nums.length; j++) {
+      for(let k = j + 1; k < nums.length; k++) {
+        const num = nums[i] + nums[j] + nums[k]
+        let isPrime = true
+        
+        for (let l = 2; l * l <= num; l++) {
+          if (num % l === 0) {
+            isPrime = false
+            break;
+          }
+        }
+        
+        if (isPrime) {
+          count++
+        }
+      }
+    }
+  }
+    
+  return count
+}
