b9f51c37ee41853ae3d33b19e3ecc7c380aa3026
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-01-17 14:35:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,6 +1,5 @@
 function solution(x) {
-    let splitedX = String(x).split('').map(el => parseInt(el))
-    const sum = splitedX.reduce((acc, cur) => acc + cur, 0)
+  const sum = `${x}`.split('').reduce((acc, cur) => acc + Number(cur), 0);
 
-    return x % sum === 0
+  return !(x % sum)
 }
