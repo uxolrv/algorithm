a350b78db3d791f4cfe9a9e0782f0a57c4dc00e8
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-01-19 11:25:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,15 +1,19 @@
+let count = 0;
+
 function solution(num) {
-    let count = 0;
-    while (num > 1) {
-		if (count === 500) {
-            return -1
-        } else if (num % 2 === 0) {
-            num = num / 2
-            count++
-        } else if (num % 2 === 1) {
-            num = num * 3 + 1
-            count++
-        }
-    }
-  return count
+  if (num === 1) return 0;
+
+  return collatz(num);
+}
+
+function collatz(num) {
+  if (num === 1) return count;
+  if (500 <= count) return -1;
+
+  if (num % 2 === 0) num /= 2;
+  else num = num * 3 + 1;
+  
+  count++;
+  
+  return collatz(num);
 }
