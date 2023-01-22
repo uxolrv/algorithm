4d30afecb159e13490123059d6598cddcbcdf581
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-01-22 19:59:43 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,6 @@
 function solution(phone_number) {
   const hiddenLength = phone_number.length - 4;
-  const hidden = '*'.repeat(hiddenLength);
+  const hiddenNums = '*'.repeat(hiddenLength) + phone_number.slice(hiddenLength);
 
-  return hidden + phone_number.slice(hiddenLength);
+  return hiddenNums;
 }
