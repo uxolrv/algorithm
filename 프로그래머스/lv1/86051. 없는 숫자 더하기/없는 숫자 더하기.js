a5c017dee477b1e97c1819918dee7c62e4794d56
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-01-28 01:06:00 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,6 @@
 function solution(numbers) {
-    const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const result = num.filter(el => !numbers.includes(el))
-    return result.reduce((acc, cur) => acc + cur) 
+  const zeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const missingNums = zeroToNine.filter((el) => !numbers.includes(el));
+
+  return missingNums.reduce((acc, cur) => acc + cur);
 }
