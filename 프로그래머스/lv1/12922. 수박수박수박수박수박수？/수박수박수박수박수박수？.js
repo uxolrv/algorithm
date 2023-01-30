f8f0ee7cfe45f7668cceac2e95b750cc00bd978c
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-01-30 22:16:22 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    let result = ''
-    // i가 홀수면 수, 짝수면 박
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 === 1) result += '수'
-        else result += '박'
-    }
-    return result
+  return n % 2 === 0 ? '수박'.repeat(n / 2) : '수박'.repeat(n / 2) + '수';
 }
