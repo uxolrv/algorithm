71896710b8ae0874c377f1b650899ec84386dae7
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-02-04 00:15:22 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,21 +1,11 @@
 function solution(left, right) {
-    let result = 0;
-
-    for (let i = left; i <= right; i++) {
-        let count = 1; // 자기 자신은 무조건 약수
-
-        for (let j = 1; j <= i/2; j++) {
-          if (i % j === 0) {
-            count++
-          }
-        }
-
-      if (count % 2 === 0) {
-        result += i
-      } else {
-        result -= i
-      }
+  let sum = 0;
+  for (let i = left; i <= right; i++) {
+    let count = 1;
+    for (let j = 1; j <= i / 2; j++) {
+      if (i % j === 0) count++;
     }
-    
-  return result
+    sum += count % 2 === 0 ? i : -i;
+  }
+  return sum;
 }
