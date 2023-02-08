16acc30bca8ce18ddb2ec4c4dc026d2ce3525f92
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-02-08 14:55:58 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,18 +1,19 @@
 function solution(n, words) {
-  const previousWords = [words[0]];
+  const previousWords = [words[0]]; // 이미 말한 단어들
   let turn = 1;
 
   for (let i = 1; i < words.length; i++) {
-    let currentFirst = words[i][0];
-    let previousLast = previousWords[i - 1][previousWords[i - 1].length - 1];
+    const currentFirst = words[i][0];
+    const previousLast = previousWords[i - 1][previousWords[i - 1].length - 1];
 
     if (i % n === 0) turn++;
-
-    if (currentFirst !== previousLast || words[i].length === 1 || previousWords.includes(words[i])) {
-      return [(i % n) + 1, turn];
+    
+    if (currentFirst !== previousLast || previousWords.includes(words[i])) {
+      return [(i % n) + 1, turn]; // 0번째 인덱스부터 첫번째 사람이기 때문에 +1
     }
 
     previousWords[i] = words[i];
   }
+
   return [0, 0];
 }
