3c9e87dafa2bd7200b81a8045ce952a1882e0817
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-02-15 22:01:55 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.31 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,7 +1,6 @@
 process.stdin.setEncoding('utf8');
-process.stdin.on('data', data => {
-    const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
+process.stdin.on('data', (data) => {
+  const [a, b] = data.split(' ').map(Number);
 
-    console.log(`${"*".repeat(a)}\n`.repeat(b));
+  console.log(`${'*'.repeat(a)}\n`.repeat(b));
 });
