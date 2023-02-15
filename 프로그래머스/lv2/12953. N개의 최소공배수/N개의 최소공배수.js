dd93371289ec31858fe62a48b7d0dc73b651c305
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-02-15 21:58:06 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -4,12 +4,5 @@ function gcd(a, b) {
 }
 
 function solution(arr) {
-  let current = arr[0];
-
-  // a와 b의 최소공배수 = a * b / a와 b의 최대공약수
-  for (let i = 0; i < arr.length - 1; i++) {
-    current *= arr[i + 1] / gcd(current, arr[i + 1]);
-  }
-
-  return current;
+  return arr.reduce((acc, cur) => (acc * cur) / gcd(acc, cur));
 }
