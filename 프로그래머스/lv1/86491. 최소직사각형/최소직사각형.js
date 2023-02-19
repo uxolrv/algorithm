30fdc0d81cab4e7073b40eba363c2bf89398d9d5
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-02-19 16:55:47 +0900

    [level 1] Title: 최소직사각형, Time: 5.64 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,10 +1,7 @@
 function solution(sizes) {
-    let w = 0;
-    let h = 0;
-  	sizes.map(el => {
-      el.sort((a,b) => a - b)
-      if (el[0] > h) h = el[0]
-      if (el[1] > w) w = el[1]
-    })
-  return w * h
+  sizes.map((size) => size.sort((a, b) => b - a));
+
+  const row = Math.max(...sizes.map((size) => size[0]));
+  const column = Math.max(...sizes.map((size) => size[1]));
+  return row * column;
 }
