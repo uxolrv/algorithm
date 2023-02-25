16809b8000805c2ac819700dcf380c3432ba2cbb
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-02-25 18:16:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -2,8 +2,8 @@ function solution(n) {
   const numOfCases = [1, 1];
 
   for (let i = 2; i <= n; i++) {
-    numOfCases[i] = numOfCases[i - 2] + (numOfCases[i - 1] % 1234567);
+    numOfCases[i] = (numOfCases[i - 2] + numOfCases[i - 1]) % 1234567;
   }
 
-  return numOfCases[n] % 1234567;
+  return numOfCases[n];
 }
