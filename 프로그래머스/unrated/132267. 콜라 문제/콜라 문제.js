7af8a10c0a24826a9dc9389667d21126ce4b1138
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-03-02 18:14:29 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,18 +1,12 @@
-function trade(a, b, empty, count) {
-  // 남은 병이 a보다 작으면 더이상 교환 불가
-  if (empty < a) {
-    return count;
-  }
-  // 교환한 병의 개수
-  const changed = b * Math.floor(empty / a)
-  empty = changed + (empty % a);
-  count += changed;
-  
-  return trade(a, b, empty, count);
-}
-
-
 function solution(a, b, n) {
   let count = 0;
-  return trade(a, b, n, count);
+  let change = 0;
+
+  while (n >= a) {
+    change = ~~(n / a) * b;
+    count += change;
+    n = (n % a) + change;
+  }
+
+  return count;
 }
