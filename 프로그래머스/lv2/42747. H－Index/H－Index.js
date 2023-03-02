e520ab4cdad70be1df2e78ff87ac0bbd2b537545
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-03-02 10:53:19 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,14 +1,9 @@
-
 function solution(citations) {
-  let result = citations.length;
-  citations.sort((a, b) => a - b);
+  citations.sort((a, b) => b - a);
 
   for (let i = 0; i < citations.length; i++) {
-    const restNum = citations.length - i - 1;
-    if (citations[i] <= restNum) {
-      result = restNum;
-    }
+    if (citations[i] <= i) return i;
   }
 
-  return result;
+  return citations.length;
 }
