e82975e0d54366e1be1f85f34ae910dfedab4282
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-03-05 23:55:38 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,14 +1,14 @@
 function solution(a, b) {
-    const dayOfWeek = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
-    let days = (a - 1) * 30 + (b - 1)
+  const dayOfWeek = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
+  let days = (a - 1) * 30 + (b - 1);
 
-    if (a !== 1 && a !== 3) {
-      if (a <= 5) days += 1
-      else if (a <= 7) days += 2
-      else if (a === 8) days += 3
-      else if (a <= 10) days += 4
-      else if (a <= 12) days += 5
-    }
+  if (a !== 1 && a !== 3) {
+    if (a <= 5) days += 1;
+    else if (a <= 7) days += 2;
+    else if (a === 8) days += 3;
+    else if (a <= 10) days += 4;
+    else if (a <= 12) days += 5;
+  }
 
-    return dayOfWeek[days % 7]
+  return dayOfWeek[days % 7];
 }
