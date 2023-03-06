782825d44be7754de66203d41e76d8409464729b
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-03-06 23:45:38 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,13 +1,8 @@
 function solution(a, b) {
-  const DAY_OF_WEEK = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
-  let dayNum = 0;
+  const dayOfWeek = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
+  const monthDays = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  if (a <= 2) {
-    dayNum = (a - 1) * 31 + b;
-  } else {
-    dayNum = Math.ceil((a - 1) / 2) * 31 + Math.floor((a - 1) / 2) * 30 - 1 + b;
-    if (a > 7 && a % 2 === 1) dayNum++; // 8월부터는 짝수 달이 31일
-  }
+  const sum = monthDays.slice(0, a).reduce((acc, cur) => acc + cur, 0) + b;
 
-  return DAY_OF_WEEK[dayNum % 7];
+  return dayOfWeek[sum % 7];
 }
