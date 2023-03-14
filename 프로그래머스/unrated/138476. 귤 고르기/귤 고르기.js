2b06e06118e4eb211bbb547e5c44b1aa0a97d3f9
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-03-14 17:57:25 +0900

    [unrated] Title: 귤 고르기, Time: 94.44 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,15 +1,11 @@
 function solution(k, tangerine) {
-  const sizes = [0];
+  const categorizing = {};
 
-  for (let size of tangerine) {
-    if (!sizes[size]) sizes[size] = 1;
-    else sizes[size]++;
-  }
-
-  sizes.sort((a, b) => b - a);
+  tangerine.map((size) => (categorizing[size] ? categorizing[size]++ : (categorizing[size] = 1)));
+  const sizes = Object.entries(categorizing).sort((a, b) => b[1] - a[1]);
 
   for (let i = 0; i < sizes.length; i++) {
-    k -= sizes[i];
+    k -= sizes[i][1];
     if (k <= 0) return i + 1;
   }
 }
