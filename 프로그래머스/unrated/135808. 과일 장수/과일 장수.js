793fc72805d0401d7f983a73ff0d88f56a7844e1
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-03-15 18:55:40 +0900

    [unrated] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,12 @@
 function solution(k, m, score) {
   let profit = 0;
-  let start = 0;
+  let last = m - 1; // 박스에 담을 마지막 과일
+
   score.sort((a, b) => b - a);
 
-  while (start <= score.length - m) {
-    profit += score[start + m - 1] * m;
-    start += m;
+  while (last < score.length) {
+    profit += score[last] * m;
+    last += m;
   }
 
   return profit;
