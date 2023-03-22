e3d3a76d8e7056a5a8ab4d2f88dbb962cf6362b9
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-03-22 17:57:52 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,16 +1,9 @@
 function solution(lottos, win_nums) {
+  // ranking의 인덱스는 일치하는 번호 수
+  // 인덱스(일치 수)에 해당하는 요소가 등수
+  const ranking = [6, 6, 5, 4, 3, 2, 1];
   let correct = 0;
   let zero = 0;
-  // 일치 번호 수: 순위
-  const ranking = {
-    6: 1,
-    5: 2,
-    4: 3,
-    3: 4,
-    2: 5,
-    1: 6,
-    0: 6,
-  };
 
   lottos.forEach((lotto) => {
     if (win_nums.includes(lotto)) correct++;
