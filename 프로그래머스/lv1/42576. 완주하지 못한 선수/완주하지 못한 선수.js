5117898597e408930e2fb1c34e8cbd55bc9caa86
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-04-06 15:32:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 36.72 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,8 @@
 function solution(participant, completion) {
-  // 완주하지 못한 선수 이름 리턴
-  const completionCount = {};
-  
-  for (let person of completion) {
-    completionCount[person] = (completionCount[person] || 0) + 1;
-  }
+  const completionCount = completion.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
 
   for (let person of participant) {
     if (completionCount[person]) completionCount[person]--;
