b050cc1406dbf1e39cfe73a84da0726ac0f88d4d
김세연 <107875909+uxolrv@users.noreply.github.com>
2023-04-09 19:06:52 +0900

    [unrated] Title: 옹알이 (2), Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,23 +1,11 @@
 function solution(babbling) {
-  const regExp = /aya|ye|woo|ma/g;
+  // (?!.*(aya|ye|woo|ma)\1) => aya, ye, woo, ma 중 어떤 문자든 2번 이상 반복되지 않아야 한다.
+  // ^(aya|ye|woo|ma)$ => aya, ye, woo, ma 중 하나로 시작하고, 끝나야 한다
+  const regExp = /^(?!.*(aya|ye|woo|ma)\1)(aya|ye|woo|ma)+$/;
   let count = 0;
 
-  outer: for (let word of babbling) {
-    const matches = word.match(regExp);
-    let previous = '';
-
-    if (!matches || word !== matches.join('')) {
-      continue;
-    }
-
-    for (let match of matches) {
-      if (match === previous) {
-        continue outer;
-      }
-      previous = match;
-    }
-
-    count++;
+  for (let word of babbling) {
+    if (regExp.test(word)) count++;
   }
 
   return count;
